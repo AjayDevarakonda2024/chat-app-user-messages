7e8236b153a98bda997b0ea5c674caa068625be1
Ajay Devarakonda <ajaydevarakonda150@gmail.com>
2025-09-27 00:52:52 +0530

    added

--- models/usermessages.js
+++ models/usermessages.js
@@ -5,7 +5,7 @@ const messageSchema = new mongoose.Schema(
         "user" : {type : String},
         "message" : {type : String},
         "likes" : {type : Number, default: 0},
-        "likedBy": {type : Array}
+        "likedBy": {type : [String], default: []}
     }
 )
 
